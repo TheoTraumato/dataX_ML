c1c7c0bbce82dd1f03c389c75d3deab7e7b0d003
TheoTraumato <65671333+TheoTraumato@users.noreply.github.com>
2021-01-04 21:10:12 +0100

    Pca new method (#1)
    * knn
    
    * pca test methode hinzugefügt (damit nicht bei Zugriff von außen immer Grafik erzeugt wird)
    
    
    Co-authored-by: maxwrede <max-wrede@gmx.de>

--- principal_component_analysis.py
+++ principal_component_analysis.py
@@ -24,34 +24,39 @@ def get_principalComponents(x_train, x_test, n_components):
     return principalComponents_train, principalComponents_test
 
 
-data_prep = Data_Preperation()
-x_train, x_test, y_train, y_test = data_prep.run()
+def __test():
+    """Private Testmethode, nur zum Testen von PCA, nicht importieren!
 
-pca_train, pca_test = get_principalComponents(x_train, x_test, 2)
 
-pca_train_df = pd.DataFrame(data=pca_train, columns=['principal component ' + str(col_number + 1)
-                                                     for col_number in range(len(pca_train[1]))])
-pca_train_df.index = x_train.index
-pca_train_df['y'] = y_train
+    """
+    data_prep = Data_Preperation()
+    x_train, x_test, y_train, y_test = data_prep.run()
 
-print(pca_train_df.tail())
+    pca_train, pca_test = get_principalComponents(x_train, x_test, 2)
 
-# Visualisierung wenn 2 PCAs behalten werden:
-if len(pca_train[1]) == 2:
-    plt.figure(figsize=(10, 10))
-    plt.xlabel('Principal Component - 1')
-    plt.ylabel('Principal Component - 2')
-    plt.title("Principal Component Analysis of Mushroom Dateset")
-    targets = [0, 1]
-    colors = ['g', 'r']
-    for target, color in zip(targets, colors):
-        indicesToKeep = y_train == target
-        plt.scatter(pca_train_df.loc[indicesToKeep, 'principal component 1'],
-                    pca_train_df.loc[indicesToKeep, 'principal component 2'],
-                    c=color, s=50)
-    plt.legend(targets, prop={'size': 15})
-    plt.show()
+    pca_train_df = pd.DataFrame(data=pca_train, columns=['principal component ' + str(col_number + 1)
+                                                         for col_number in range(len(pca_train[1]))])
+    pca_train_df.index = x_train.index
+    pca_train_df['y'] = y_train
 
-"""ERGEBNIS: Eher ungeeignet für unseren Datensatz, da zu viel Informationen verloren geht. Im ursprünglichen Datensatz
-ist die Varianz sehr gleichmeißig auf alle Features verteilt, weswegen die Principal Components wenig Varianz
-'an sich ziehen können'"""
+    print(pca_train_df.tail())
+
+    # Visualisierung wenn 2 PCAs behalten werden:
+    if len(pca_train[1]) == 2:
+        plt.figure(figsize=(10, 10))
+        plt.xlabel('Principal Component - 1')
+        plt.ylabel('Principal Component - 2')
+        plt.title("Principal Component Analysis of Mushroom Dateset")
+        targets = [0, 1]
+        colors = ['g', 'r']
+        for target, color in zip(targets, colors):
+            indicesToKeep = y_train == target
+            plt.scatter(pca_train_df.loc[indicesToKeep, 'principal component 1'],
+                        pca_train_df.loc[indicesToKeep, 'principal component 2'],
+                        c=color, s=50)
+        plt.legend(targets, prop={'size': 15})
+        plt.show()
+
+    """ERGEBNIS: Eher ungeeignet für unseren Datensatz, da zu viel Informationen verloren geht. Im ursprünglichen Datensatz
+    ist die Varianz sehr gleichmeißig auf alle Features verteilt, weswegen die Principal Components wenig Varianz
+    'an sich ziehen können'"""
