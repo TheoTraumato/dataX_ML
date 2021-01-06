1ed3ca40a8d3ddec2111c85b95c072f125352d2d
theofrielinghaus <theofrielinghaus@googlemail.com>
2021-01-06 22:56:07 +0100

    DataPreperation vollständig und kommentiert

--- data_prep.py
+++ data_prep.py
@@ -25,32 +25,37 @@ class Data_Preperation():
 
 
     def __prepare_data(self):
+        """Liest Daten aus unseren Projektdaten, die von kaggle heruntergeladen wurden. Anschließend werden Duplikate
+        entfernt, auf fehlende Werte (NaN) überprüft, und verschiedene Features mit String-Datentyp in numerische
+        umgewandelt und die abhängige Variable von den unabhängigen getrennt.
+         :return: x,y (DataFrame): Unabhängige Variablen, Abhängige Variablen
         """
-        Liest Daten aus unseren Projektdaten, die von kaggle heruntergeladen wurden. Anschließend werden Duplikate entfernt,
-        auf fehlende Werte (NaN) überprüft und die abhängige Variable von den unabhängigen getrennt.
-        :return: x,y (DataFrame): Unabhängige Variablen, Abhängige Variablen
-        """
-        # Read data
+        # Daten in DateFrame-Objekt lesen
         df = pd.read_csv('archive/WA_Fn-UseC_-Telco-Customer-Churn.csv')
         print(df)
 
-        # Data Preperation
+        # CustomerID brauchen wir nicht, wird deswegen entfernt
         df = df.drop('customerID', axis=1)
+
+        # Duplikate werden entfernt
         df.drop_duplicates(inplace=True)
         print(df.shape)
 
+        # Alle Features die 'Yes' and 'No' als Ausprägungen haben werden umgewandelt
         boolean_values = ['Partner', 'Dependents', 'PhoneService', 'PaperlessBilling', 'Churn' ]
         for column in boolean_values:
             df[column] = df[column].replace({'Yes': 1, 'No': 0})
-        print(df.head)
-        print(df.dtypes)
 
+        # Im Feature 'TotalCharges' sind die Werte als string gespeichert, werden hier umgewandelt
+        df['TotalCharges'] = df['TotalCharges'].replace({" ":'0'})
+        df['TotalCharges'] = df['TotalCharges'].astype(float)
+
+        # Alle Features werden auf NaN-Werte überprüft (es sind keine vorhanden)
         print('Relative Menge an Missing Values: ', df.isna().sum() / (len(df)) * 100)
 
-        y = df['class']
-        x = df.drop('class', axis=1)
-
-        y = y.replace({'p': 1, 'e': 0})
+        # Abhängige wird von den unabhängigen Variablen gelöst
+        y = df['Churn']
+        x = df.drop('Churn', axis=1)
 
         return x, y
 
@@ -60,15 +65,15 @@ class Data_Preperation():
 
 
         Args:
-            use_one_hot_encoding: Soll One Hot Encoding angewandet werden auf dem DataFrame
+            use_one_hot_encoding: (Boolean) Soll One Hot Encoding angewandet werden auf dem DataFrame
         :return: x_train, x_test, y_train, y_test: Trainings- und Testdaten
         """
         x, y = self.__prepare_data()
         if use_one_hot_encoding:
             x = self.__one_hot_encoding(x)
+        #print(x.head)
+        #print(x.dtypes)
 
-        # TODO: Prüfen ob Outlier Häufigkeit der Merkmale nötig ist
-        # TODO: Feature Selection mit PCA, Lasso-Regression, Ridge-Regression (Übung 4)
 
         x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=123)
 
