95aca2fce005d025e72bf3ab67fa89190ef496d4
theofrielinghaus <theofrielinghaus@googlemail.com>
2021-01-13 00:28:58 +0100

    Grid_search implementiert

--- support_vector_machine.py
+++ support_vector_machine.py
@@ -4,10 +4,23 @@ from sklearn import svm, metrics, model_selection
 data_prep = data_prep.Data_Preperation()
 x_train, x_test, y_train, y_test = data_prep.run()
 
-params = dict(kernel=['linear', 'poly', 'rbf', 'sigmoid'], C=[1.0, 0.5, 0.1, 0.05, 0.01], gamma=['scale', 'auto', 0.1, 1, 10, 20])
+params = dict(kernel=['linear', 'poly', 'rbf', 'sigmoid'], C=[1.0, 0.5, 0.1, 0.05, 0.01],
+              gamma=['scale', 'auto', 0.1, 1, 10, 20])
 
-svm_clf = svm.SVC()
-svm_clf.fit(x_train, y_train)
+
+
+f1_scorer = metrics.make_scorer(metrics.f1_score)
+grid_search = model_selection.GridSearchCV(estimator=svm.SVC, param_grid=params, verbose=2, scoring=f1_scorer,
+                                           return_train_score=True, n_jobs=-1)
+grid_search.fit(x_train, y_train)
+print(grid_search.best_params_)
+best_kernel = [grid_search.best_params_['kernel']]
+best_c = [grid_search.best_params_['C']]
+best_gamma = [grid_search.best_params_['gamma']]
+
+svm_clf = svm.SVC(kernel=best_kernel, C=best_c, gamma=best_gamma)
+svm_clf.fit(x_train,y_train)
+
 pred = svm_clf.predict(x_test)
 
 # confusion matrix
@@ -18,3 +31,5 @@ print("acuracy:", metrics.accuracy_score(y_test, pred))
 print("precision:", metrics.precision_score(y_test, pred))
 # recall score
 print("recall", metrics.recall_score(y_test, pred))
+#f1 score
+print("F1-Score", metrics.f1_score(y_test, pred))
