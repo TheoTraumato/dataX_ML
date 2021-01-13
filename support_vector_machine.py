34d8acc11a2a597def68a19393b11f51f25b9208
theofrielinghaus <theofrielinghaus@googlemail.com>
2021-01-13 10:31:48 +0100

    Grid_search implementiert

--- support_vector_machine.py
+++ support_vector_machine.py
@@ -5,7 +5,9 @@ data_prep = data_prep.Data_Preperation()
 x_train, x_test, y_train, y_test = data_prep.run()
 
 #kernel: 'linear', 'poly', 'rbf', 'sigmoid'
-params = dict(kernel=['linear'], C=[ 1.0, 0.1, 0.01, ],
+params = dict(kernel=['linear', 'poly', 'rbf', 'sigmoid'], C=[ 1.0,0.5, 0.1, 0.05, 0.01, ],
+             gamma=['scale', 'auto',  1.0, 0.1, 0.01, ])
+params_poly = dict(kernel=['poly', ], C=[ 1.0,0.5, 0.1, 0.05, 0.01, ],
              gamma=['scale', 'auto',  1.0, 0.1, 0.01, ])
 
 #grid1: {'C': 0.1, 'gamma': 'scale', 'kernel': 'linear'}
@@ -14,14 +16,14 @@ params = dict(kernel=['linear'], C=[ 1.0, 0.1, 0.01, ],
 
 f1_scorer = metrics.make_scorer(metrics.f1_score)
 grid_search = model_selection.GridSearchCV(estimator=svm.SVC(), param_grid=params, verbose=2, scoring=f1_scorer,
-                                           return_train_score=True, n_jobs=1)
-#grid_search.fit(x_train, y_train)
-#print(grid_search.best_params_)
+                                           return_train_score=True, n_jobs=2)
+grid_search.fit(x_train, y_train)
+print(grid_search.best_params_)
 #best_kernel = [grid_search.best_params_['kernel']]
 #best_c = [grid_search.best_params_['C']]
 #best_gamma = [grid_search.best_params_['gamma']]
 
-svm_clf = svm.SVC(kernel='linear', C=0.1, gamma='scale')
+svm_clf = svm.SVC(**grid_search.best_params_)
 svm_clf.fit(x_train, y_train)
 
 pred = svm_clf.predict(x_test)
@@ -29,7 +31,7 @@ pred = svm_clf.predict(x_test)
 # confusion matrix
 print(metrics.confusion_matrix(y_test, pred))
 # accuracy
-print("acuracy:", metrics.accuracy_score(y_test, pred))
+print("accuracy:", metrics.accuracy_score(y_test, pred))
 # precision score
 print("precision:", metrics.precision_score(y_test, pred))
 # recall score
