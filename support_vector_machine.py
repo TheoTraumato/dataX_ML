079800fbe87b2d34bff3e498538057ef5e49e35f
theofrielinghaus <theofrielinghaus@googlemail.com>
2021-01-13 16:03:38 +0100

    Parameter für Sigmoid, Linear und RBF Kernel optimiert.

--- support_vector_machine.py
+++ support_vector_machine.py
@@ -1,23 +1,40 @@
 import data_prep
 from sklearn import svm, metrics, model_selection
 
+from principal_component_analysis import get_principalComponents
+
 data_prep = data_prep.Data_Preperation()
 x_train, x_test, y_train, y_test = data_prep.run()
+#x_train, x_test = get_principalComponents(x_train, x_test, 2)
 
 # kernel: 'linear', 'poly', 'rbf', 'sigmoid'
-params = dict(kernel=['linear', 'rbf', 'sigmoid'], C=[1.0, 0.5, 0.1, 0.05, 0.01, ],
-              gamma=['scale', 'auto', 1.0, 0.1, 0.01, ])
-params_poly = dict(kernel=['poly'], C=[1.0, 0.5, 0.1, 0.05, 0.01, ],
-                   gamma=['scale', 'auto', 1.0, 0.1, 0.01, ])
+params_linear = dict(kernel=[ 'linear'], C=[ 1.0, 0.1, 0.01, 0.001, 0.0001 ],)
+params_poly = dict(kernel=['poly'], C=[ 0.1, 0.05, 0.01, 0.001 ],
+                   gamma=['scale', 'auto',  0.5, 0.1, 0.01 ], degree=[2,3,4])
+params_sigmoid = dict(kernel=['sigmoid'], C=[10, 0.1, 0.05, 0.01, ],
+                   gamma=['scale', 'auto', 10, 1.0,0.5, 0.1, 0.01 ])
 
 # grid1: {'C': 0.1, 'gamma': 'scale', 'kernel': 'linear'}
 # grid2: {'C': 0.05, 'gamma': 'scale', 'kernel': 'linear'}
+# grid3: (no more f1 scorer for gridsearch): {'C': 1.0, 'gamma': 'scale', 'kernel': 'rbf'}
+# grid4: {'C': 10, 'gamma': 0.01, 'kernel': 'rbf'}
+# grid5: {'C': 100, 'gamma': 0.001, 'kernel': 'rbf'}
+# grid6: {'C': 500, 'gamma': 0.001, 'kernel': 'rbf'}
+#grid 7: {'C': 500, 'gamma': 0.001, 'kernel': 'rbf'} # accuracy 80,14 precision 70,42, recall 50, f1 58%
+
+#linear kernel:
+#grid 1: {'C': 0.01, 'kernel': 'linear'}
+#grid 2: {'C': 0.01, 'kernel': 'linear'} # accuracy 79,97 precision 67,92%, recall 54,5% f1% 60%
+
+#sigmoid_kernel: {'C': 0.1, 'gamma': 'scale', 'kernel': 'sigmoid'} acc: 79,52%, prec 66,83% recall 53,95%
 
 
 f1_scorer = metrics.make_scorer(metrics.f1_score)
-grid_search = model_selection.GridSearchCV(estimator=svm.SVC(), param_grid=params, verbose=2, scoring=f1_scorer,
-                                           return_train_score=True, n_jobs=2)
+grid_search = model_selection.GridSearchCV(estimator=svm.SVC(), param_grid=params_sigmoid, verbose=2,
+                                           return_train_score=True, n_jobs=1)
 grid_search.fit(x_train, y_train)
+print('Mean cross-validated score of the best_estimator: ', grid_search.best_score_)
+print('Best Estimator: ' , grid_search.best_estimator_)
 print(grid_search.best_params_)
 # best_kernel = [grid_search.best_params_['kernel']]
 # best_c = [grid_search.best_params_['C']]
