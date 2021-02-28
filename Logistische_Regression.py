24b28482d48887d0bd148ea9ef4bd9c8bb75259d
maxwrede <max-wrede@gmx.de>
2021-02-28 14:45:06 +0100

    Stand 28.02.

--- Logistische_Regression.py
+++ Logistische_Regression.py
@@ -4,18 +4,19 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score
 from sklearn.model_selection import GridSearchCV
 from Confusion_Matrix import plot_confusion_matrix
+from Roc_curve import plot_roc_curve
 
 # Data preparation with One Hot Encoding:
 import data_prep
 
 data_prep = data_prep.Data_Preperation()
-x_train, x_test, y_train, y_test = data_prep.run(standardize_data=True, oversampling=True)
+x_train, x_test, y_train, y_test = data_prep.run(standardize_data=True, oversampling=False)
 
 # Logistic Regression Basis Model:
 logreg = LogisticRegression(penalty="none")
 
 # Train model:
-logreg.fit(x_train, y_train)
+logreg_basis = logreg.fit(x_train, y_train)
 
 # predict using x_test:
 y_pred = logreg.predict(x_test)
@@ -25,6 +26,7 @@ print("Accuracy for basis model: ", accuracy_score(y_test, y_pred))
 print("Precision for basis model: ", precision_score(y_test, y_pred))
 print("Recall for basis model: ", recall_score(y_test, y_pred))
 print("F1-score for basis model: ", sklearn.metrics.f1_score(y_test, y_pred))
+print("ROC for basis model: ", plot_roc_curve(logreg_basis, x_test, y_test))
 logreg_matrix = confusion_matrix(y_test, y_pred)
 plot_confusion_matrix(logreg_matrix, classes=['churn=0','churn=1'],normalize= False,  title='Confusion matrix basis model')
 
@@ -36,10 +38,10 @@ logreg = LogisticRegression()
 parameters_logreg = {"C" : np.logspace(-6, 6, 50), "penalty" : ['l1', "l2"], "solver" : ["liblinear"]}
 
 # GridSearch
-clf_logreg = GridSearchCV(logreg, parameters_logreg, scoring='neg_log_loss', cv=10)
+logreg = GridSearchCV(logreg, parameters_logreg, scoring='roc_auc', cv=10)
 
 # Train model:
-clf_logreg.fit(x_train, y_train)
+clf_logreg = logreg.fit(x_train, y_train)
 
 # predict using x_test:
 y_pred_logreg = clf_logreg.predict(x_test)
@@ -50,5 +52,6 @@ print("Accuracy for logreg: ", accuracy_score(y_test, y_pred_logreg))
 print("Precision for logreg: ", precision_score(y_test, y_pred_logreg))
 print("Recall for logreg: ", recall_score(y_test, y_pred_logreg))
 print("F1-score for logreg: ", sklearn.metrics.f1_score(y_test, y_pred_logreg))
+print("ROC for logreg: ", plot_roc_curve(clf_logreg, x_test, y_test))
 logreg_matrix = confusion_matrix(y_test, y_pred_logreg)
 plot_confusion_matrix(logreg_matrix, classes=['churn=0','churn=1'],normalize= False,  title='Confusion matrix logreg optimized')
--- knn_new.py
+++ knn_new.py
@@ -1,6 +1,9 @@
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
+from Roc_curve import plot_roc_curve
+from sklearn.metrics import classification_report, confusion_matrix
+from Confusion_Matrix import plot_confusion_matrix
 
 # Daten erstellen mit One Hot Encoding:
 from data_prep import Data_Preperation
@@ -9,80 +12,39 @@ data_prep = Data_Preperation()
 x_train, x_test, y_train, y_test = data_prep.run(standardize_data=True, oversampling=True)
 
 # KNN
-"""
-Berechnet den Abstand eines neuen Datenpunktes x zu allen anderen Trainingsdatenpunkten.
-K ist dabei die Anzahl der nächstgelegenen Punkte zu x.
-Mittels Cross Validation und anhand des F1 Scores wird das optimale k bestimmt
-"""
 
 #basis modell:
-# train model:
 knn = KNeighborsClassifier()
-knn.fit(x_train, y_train)
-y_pred_basis = knn.predict(x_test)
+knn_basis = knn.fit(x_train, y_train)
+y_pred_basis = knn_basis.predict(x_test)
 
 print("Accuracy for knn_basis: ", accuracy_score(y_test, y_pred_basis))
 print("Precision for knn_basis: ", precision_score(y_test, y_pred_basis))
 print("Recall for knn_basis: ", recall_score(y_test, y_pred_basis))
 
+# confusion matrix basis:
+knn_matrix = confusion_matrix(y_test, y_pred_basis)
+plot_confusion_matrix(knn_matrix, classes=['churn=0','churn=1'],normalize= False,  title='Confusion matrix')
+
 # CV für Ermittlung des optimalen k
 params = {"n_neighbors": range(1,100)}
-clf = GridSearchCV(knn, params, scoring="neg_log_loss", cv=10)
+clf = GridSearchCV(knn, params, scoring="roc_auc", cv=10)
 clf.fit(x_train, y_train)
 best_k = clf.best_params_["n_neighbors"]
 
 # train model:
 knn = KNeighborsClassifier(n_neighbors=best_k)
-knn.fit(x_train, y_train)
-y_pred = knn.predict(x_test)
+knn_clf = knn.fit(x_train, y_train)
+y_pred = knn_clf.predict(x_test)
 
 # evaluation
-from sklearn.metrics import classification_report, confusion_matrix
 score = knn.score(x_test, y_test)
 print("Accuracy for knn: ", accuracy_score(y_test, y_pred))
 print("Precision for knn: ", precision_score(y_test, y_pred))
 print("Recall for knn: ", recall_score(y_test, y_pred))
 print("Best_k used = ", best_k)
+print("ROC for opt. model: ", plot_roc_curve(knn_clf, x_test, y_test))
 
 # confusion matrix:
-from Confusion_Matrix import plot_confusion_matrix
 knn_matrix = confusion_matrix(y_test, y_pred)
 plot_confusion_matrix(knn_matrix, classes=['churn=0','churn=1'],normalize= False,  title='Confusion matrix')
-
-"""
-Evaluation: standardize_data=False, oversampling=False:
-Accuracy for knn:  0.7637010676156584
-Precision for knn:  0.60828025477707
-Recall for knn:  0.4775
-Best_k used =  98
-Confusion matrix, without normalization
-[[882 123]
- [209 191]]
- 
-Evaluation: standardize_data=True, oversampling=False:
-Accuracy for knn:  0.7622775800711744
-Precision for knn:  0.6050955414012739
-Recall for knn:  0.475
-Best_k used =  76
-Confusion matrix, without normalization
-[[881 124]
- [210 190]] 
- 
-Evaluation: standardize_data=False, oversampling=True:
-Accuracy for knn:  0.7615658362989324
-Precision for knn:  0.5757575757575758
-Recall for knn:  0.6175
-Best_k used =  81
-Confusion matrix, without normalization
-[[823 182]
- [153 247]]
- 
-Evaluation: standardize_data=True, oversampling=True:
-Accuracy for knn:  0.7601423487544484
-Precision for knn:  0.5704697986577181
-Recall for knn:  0.6375
-Best_k used =  89
-Confusion matrix, without normalization
-[[813 192]
- [145 255]]
- """
